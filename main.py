c9c867046fa97c9db0bba94a6cd5c2ac93fe3311
Michael Meng <m3meng@uwaterloo.ca>
2020-02-12 23:17:29 -0500

    added simple query, changed read me to include some design questions

--- main.py
+++ main.py
@@ -1,13 +1,75 @@
-from flask import Flask
-import sqlite3
-conn = sqlite3.connect('hackers.db')
+# from flask import Flask
+# import sqlite3
+# conn = sqlite3.connect('hackers.db')
 
+# app = Flask(__name__)
+
+
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, World! Michael Meng'
+
+from __future__ import print_function
+from flask import Flask, g, jsonify
+import sqlite3
+import os
+import sys
+
+DATABASE = "./hackers.db"
+
+# Create app
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+# check if the database exist, if not create the table and insert a few lines of data
+# if not os.path.exists(DATABASE):
+conn = sqlite3.connect(DATABASE)
+# cur = conn.cursor()
+# cur.execute("CREATE TABLE users (fname TEXT, lname TEXT, age INTEGER);")
+# conn.commit()
+# cur.execute("INSERT INTO users VALUES('Mike', 'Tyson', '40');")
+# cur.execute("INSERT INTO users VALUES('Thomas', 'Jasper', '40');")
+# cur.execute("INSERT INTO users VALUES('Jerry', 'Mouse', '40');")
+# cur.execute("INSERT INTO users VALUES('Peter', 'Pan', '40');")
+# conn.commit()
+# conn.close()
 
 
+# helper method to get the database since calls are per thread,
+# and everything function is a new thread when called
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
 
 
+# helper to close
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+@app.route("/")
+def start():
+    return 'start app :)'
+
+
+@app.route("/users")
+def index():
+    print('before get_db cursor')
+    cur = get_db().cursor()
+    res = cur.execute("select * from users")
+    return jsonify(data=res.fetchall())
+    # return 'check your console :)'
+
+
+if __name__ == "__main__":
+    """
+        Use python sqlite3 to create a local database, insert some basic data and then
+        display the data using the flask templating.
+
+        http://flask.pocoo.org/docs/0.12/patterns/sqlite3/
+    """
+    app.run(debug=True)
--- readme.md
+++ readme.md
@@ -1,5 +1,15 @@
 # Hack the North 2020 Backend Boilerplate
 
-This boilerplate contains a sqlite + Flask install. The current directory is mounted as a volume under `/home/api` so that you do not have to rebuild the image every time. Building and running the image will start the Flask server on port 5000.
+:rice_cracker::rice_cracker::rice_cracker::rice_cracker::rice_cracker::rice_cracker::rice_cracker::rice_cracker::rice_cracker:
 
-Good luck!
+(This boilerplate contains a sqlite + Flask install. The current directory is mounted as a volume under `/home/api` so that you do not have to rebuild the image every time. Building and running the image will start the Flask server on port 5000.)
+
+#Documenting Design Decisions
+
+1. setting up the sqlite database, need to parse JSON file into sqlite
+
+1.1) What's the best way to set up the table? What kind of relationships do we have? etc?
+
+2. write the 3 user endpoints. use the sqlite database for querying, joining tables, etc
+
+3. idk
