ed60224e1bb3ffb3d8324554e086ef5875c0efd4
Michael Meng <m3meng@uwaterloo.ca>
2020-02-17 23:29:54 -0500

    finish get event info route

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ def get_users():
         FROM users AS u
         LEFT JOIN userEvents AS ue ON u.userID = ue.userID
         LEFT JOIN events AS e ON ue.eventID = e.eventID
-        GROUP BY u.userID 
+        GROUP BY u.userID
         ''')
     columns = [column[0] for column in cur.description]
     results = []
@@ -97,42 +97,39 @@ def get_user(id):
     # input validation
     # regex checks for positive integer, need to implement error check where there is question mark at end of <id>
     if re.search('^[0-9]*[1-9][0-9]*$', id) == None:
-        abort(400, "'userID' must be positive interger number")
-        # return Response("ERROR: 'userID' must be positive interger number", status=400)
+        abort(400, "ERROR: 'userID' must be positive interger number")
 
     cur = conn.cursor()
     cur.execute('''
         SELECT company, email, latitude, longitude, name, phone, picture,
         GROUP_CONCAT('{"name":"' || eventName || '"}') events
-        FROM users AS u 
+        FROM users AS u
         LEFT JOIN userEvents AS ue ON u.userID = ue.userID
         LEFT JOIN events AS e ON ue.eventID = e.eventID
         WHERE u.userID = ?
-        GROUP BY u.userID 
+        GROUP BY u.userID
         ''', [id])
+
+    all_rows = cur.fetchall()
+    if all_rows == []:
+        return Response("[], eventID does not exist", status=200)
+
     columns = [column[0] for column in cur.description]
-    results = []
-    for row in cur.fetchall():
-        dictionary = dict(zip(columns[:7], row))
-        # for events column create dictionary for events:
+    for row in all_rows:
+        user_dict = dict(zip(columns[:7], row))
+        # for events column create user_dict for events:
         dict_events = []
         event_list = ast.literal_eval(row['events'])
         if len(event_list) == 1:
             dict_events.append(event_list)
-            dictionary["events"] = dict_events
+            user_dict["events"] = dict_events
         else:
             for event in event_list:
                 dict_events.append(event)
-            dictionary["events"] = dict_events
-        results.append(dictionary)
-    # there might be easier way to check for [] results before hand, implement as tech debt later on
-    if results == []:
-        return Response("[], userID does not exist")
-
-    return Response(json.dumps(results),  mimetype='application/json')
+            user_dict["events"] = dict_events
+    return Response(json.dumps(user_dict),  mimetype='application/json', status=200)
 
 # API route handler to get user information within a latitude and longitude range
-# input rule, only put in float point numbers, no letters
 @app.route("/users/params", methods=['GET'])
 def get_user_in_range():
 
@@ -151,11 +148,11 @@ def get_user_in_range():
     cur.execute('''
         SELECT company, email, latitude, longitude, name, phone, picture,
         GROUP_CONCAT('{"name":"' || eventName || '"}') events
-        FROM users AS u 
+        FROM users AS u
         LEFT JOIN userEvents AS ue ON u.userID = ue.userID
         LEFT JOIN events AS e ON ue.eventID = e.eventID
         WHERE ROUND(ABS(u.latitude - ?), 4) <= ABS(?)  AND ROUND(ABS(u.longitude - ?), 4) <= ABS(?)
-        GROUP BY u.userID 
+        GROUP BY u.userID
         ''', [latitude, loc_range, longitude, loc_range])
 
     columns = [column[0] for column in cur.description]
@@ -175,9 +172,49 @@ def get_user_in_range():
             dictionary["events"] = dict_events
         results.append(dictionary)
 
-    return Response(json.dumps(results),  mimetype='application/json')
+    return Response(json.dumps(results),  mimetype='application/json', status=200)
 
 # ----------------------EVENTS ENDPOINTS----------------------
+
+# API route handler to get information for a specific event
+@app.route("/events/<id>", methods=['GET'])
+def get_event(id):
+    # input validation
+    # regex checks for positive integer, need to implement error check where there is question mark at end of <id>
+    if re.search('^[0-9]*[1-9][0-9]*$', id) == None:
+        abort(400, "ERROR: 'eventID' must be positive interger number")
+
+    cur = conn.cursor()
+    cur.execute('''
+        SELECT e.eventID, e.eventName, u.name, u.company, u.email, u.latitude, u.longitude, u.phone, u.picture
+        FROM events AS e
+        JOIN userEvents AS ue ON e.eventID = ue.eventID
+        JOIN users AS u ON ue.userID = u.userID
+        WHERE e.eventID = ?
+        ''', [id])
+
+    all_rows = cur.fetchall()
+    if all_rows == []:
+        return Response("[], eventID does not exist", status=200)
+
+    dictionary = {}
+    dict_users = []
+
+    columns = [column[0] for column in cur.description]
+    dictionary["eventName"] = all_rows[0][1]
+    dictionary["eventID"] = id
+
+    for row in all_rows:
+        # for every row, need to extract user objects in dict form, and append to a list that will be under "attendees" key in dictionary
+        # row_literal = ast.literal_eval(row)
+        row_slice = []
+        for i in range(2, 9):
+            row_slice.append(row[i])
+        user_dict = dict(zip(columns[2:], row_slice))
+        dict_users.append(user_dict)
+
+    dictionary["attendees"] = dict_users
+    return Response(json.dumps(dictionary),  mimetype='application/json', status=200)
 
 
 if __name__ == "__main__":
